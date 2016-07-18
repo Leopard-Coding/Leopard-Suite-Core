61bb3d5b15701bc5ee4a89c6771e1b6b013d4241
Julian Pfeil <wardz0112358@gmail.com>
2016-07-18 19:54:27 +0200

    Add dbconfig.inc.php

---
+++ dbconfig.inc.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Contains database-connection data
+ *
+ * @copyright ©Leopard
+ * @license http://creativecommons.org/licenses/by-nd/4.0/ CC BY-ND 4.0
+ *
+ * @author Julian Pfeil
+ */ 
+namespace LSC;
+ 
+$dbHost = '';
+$dbName = '';
+$dbUser = '';
+$dbPassword = '';

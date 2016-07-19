5600ebcfdc329ea63caccfcb149c010878870b62
Julian Pfeil <wardz0112358@gmail.com>
2016-07-19 07:22:22 +0200

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -10,13 +10,14 @@
 namespace LSC;
 
 use LSC\lib\system\LSC,
-	LSC\lib\extension\Leopard\HuntingRouter;
+	LSC\lib\extension\Leopard\HuntingRouter,
+	LSC\lib\system\database\Database;
 
 ob_start();
 session_start();
 
 define('__LSC_ABSOLUTE_DIR__', str_replace('\\', '/', dirname(__FILE__)).'/');
-define('__LSC_INSTALLATON_NUMBER__', 1);
+define('__LSC_INSTALLATION_NUMBER__', 1);
 
 require_once(__LSC_ABSOLUTE_DIR__.'lib/system/LSC.class.php');
 LSC::loadAutoloadHandler();
@@ -25,3 +26,5 @@ LSC::loadRouting();
 
 define('__LSC_DIR__', HuntingRouter::getURL());
 define('__LSC_RELATIVE_DIR__', HuntingRouter::getCurrentPath(true));
+
+Database::connect();

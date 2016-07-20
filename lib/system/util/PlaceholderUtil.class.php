60d8060f82f80c0c7384941f05cbf7176677c619
Julian Pfeil <wardz0112358@gmail.com>
2016-07-20 17:17:19 +0200

    Update PlaceholderUtil.class.php

--- lib/system/util/PlaceholderUtil.class.php
+++ lib/system/util/PlaceholderUtil.class.php
@@ -1,25 +1,8 @@
 <?php
-/**
- * Replaces placeholder in content
- *
- * @copyright ©Leopard
- * @license http://creativecommons.org/licenses/by-nd/4.0/ CC BY-ND 4.0
- *
- * @author Julian Pfeil
- */ 
 namespace LSC\lib\system\util;
 
 class PlaceholderUtil
 {
-	/**
-	 * Replaces placeholder in content
-	 *
-	 * @param array $Placeholders Array where keys are placeholder-names and values are the replacements
-	 * @param string $Source Path to file with content or directly the content
-	 * @param boolean $File If content is read out of file
-	 *
-	 * @return void|string $Source IF content is not read out of file, returns edited content
-	 */ 
 	public static function replace(array $Placeholders, $Source, $File = true)
 	{
 		if ((bool) $File) {

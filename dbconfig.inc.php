77209c277542df379984fe639e0f5505e342a409
Julian Pfeil <wardz0112358@gmail.com>
2016-07-20 17:11:51 +0200

    Update dbconfig.inc.php

--- dbconfig.inc.php
+++ dbconfig.inc.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Contains database-connection data
- *
- * @copyright ©Leopard
- * @license http://creativecommons.org/licenses/by-nd/4.0/ CC BY-ND 4.0
- *
- * @author Julian Pfeil
- */ 
 namespace LSC;
  
 $dbHost = '';

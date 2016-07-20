d4405e780c536b9a1f09e6db891df4a6e2c0341e
Julian Pfeil <wardz0112358@gmail.com>
2016-07-20 17:14:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php
-/**
- * Leopard Suite Core
- *
- * @version 0.1.0-alpha
- * @copyright ©Leopard
- * @license http://creativecommons.org/licenses/by-nd/4.0/ CC BY-ND 4.0
- *
- * @author Julian Pfeil
- */
 namespace LSC;
 
 require('config.inc.php');
